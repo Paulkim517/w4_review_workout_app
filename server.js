0fa6fbb8210ba24d01d4b9f0259292e37bf79716
cameronjacoby <cameron.jacoby@gmail.com>
2015-07-16 15:39:39 -0700

    auth

--- server.js
+++ server.js
@@ -2,13 +2,47 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    Log = require('./models/log');
+    Log = require('./models/log'),
+    User = require('./models/user'),
+    session = require('express-session');
 
 mongoose.connect('mongodb://localhost/do_you_even_lift');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'SuperSecretCookie',
+  cookie: { maxAge: 60000 }
+}));
+
+// middleware to manage sessions
+app.use('/', function (req, res, next) {
+  // saves userId in session for logged-in user
+  req.login = function (user) {
+    req.session.userId = user.id;
+  };
+
+  // finds user currently logged in based on `session.userId`
+  req.currentUser = function (callback) {
+    User.findOne({_id: req.session.userId}, function (err, user) {
+      req.user = user;
+      callback(null, user);
+    });
+  };
+
+  // destroy `session.userId` to log out user
+  req.logout = function () {
+    req.session.userId = null;
+    req.user = null;
+  };
+
+  next();
+});
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/views/index.html');
@@ -17,6 +51,37 @@ app.get('/', function (req, res) {
 app.get('/api/logs', function (req, res) {
   Log.find(function (err, logs) {
     res.json(logs);
+  });
+});
+
+app.get('/signup', function (req, res) {
+  res.sendFile(__dirname + '/public/views/signup.html');
+});
+
+app.post('/users', function (req, res) {
+  var newUser = req.body.user;
+
+  User.createSecure(newUser.email, newUser.password, function (err, user) {
+    res.redirect('/login');
+  });
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/public/views/login.html');
+});
+
+app.post('/login', function (req, res) {
+  var userData = req.body.user;
+
+  User.authenticate(userData.email, userData.password, function (err, user) {
+    req.login(user);
+    res.redirect('/profile');
+  });
+});
+
+app.get('/profile', function (req, res) {
+  req.currentUser(function (err, user) {
+    res.render('profile', {currentUser: user});
   });
 });
 
